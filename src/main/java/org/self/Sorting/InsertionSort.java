4e5c237cb959d148c60dc2bac7fb9acf405b0bc8
trisha123shetty <trishashetty33@gmail.com>
2026-03-04 08:16:15 +0530

    Insertion sort

---
+++ src/main/java/org/self/Sorting/InsertionSort.java
@@ -0,0 +1,16 @@
+package org.self.Sorting;
+class insertionSort {
+    // Please change the array in-place
+    public void insertionSort(int arr[]) {
+        // code here
+        for(int i=1;i<arr.length;i++){
+            int j=i;
+            while(j>0&&arr[j-1]>arr[j]){
+                int temp=arr[j-1];
+                arr[j-1]=arr[j];
+                arr[j]=temp;
+                j--;
+            }
+        }
+    }
+}
